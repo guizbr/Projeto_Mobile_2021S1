68839bcc33d116a88dbe664b01b53c34af7ff563
guizbr <guilherme1290@outlook.com.br>
2021-05-26 01:58:35 -0300

    notificacao push

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +15,9 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    OneSignal.shared.init('7a5bcb46-9de7-4e74-897e-6e0a9db4e723');
+    //OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.none);
+    //OneSignal.shared.setNotificationReceivedHandler((OSNotification notification) { });
     return MaterialApp(
       title: 'Chat Flutter',
       theme: ThemeData(
